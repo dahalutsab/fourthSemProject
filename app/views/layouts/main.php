3ba8c3c71f7c4573f1a8cd3cf80261c76e41310b
dahalutsab <dlutsab2120@gmail.com>
2024-03-11 09:09:30 +0545

    managed the mistake card

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="/public/assets/css/home.css">
     <link rel="stylesheet" href="/public/assets/css/artists.css">
     <link rel="stylesheet" href="/public/assets/css/artist-card.css">
+    <link rel="stylesheet" href="/public/assets/css/artist-details.css">
     <link rel="stylesheet" href="/public/assets/css/footer.css">
     <link rel="stylesheet" href="/public/assets/css/contactUs.css">
     <link rel="stylesheet" href="/public/assets/css/style.css">
