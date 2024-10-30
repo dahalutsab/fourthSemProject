b17714ec40e9b6585e27324827d3556e8df88531
dahalutsab <dlutsab2120@gmail.com>
2024-10-30 13:07:59 +0545

    changed the error in routes

--- public/index.php
+++ public/index.php
@@ -11,8 +11,7 @@ require_once __DIR__ . '/../config/session.php';
 require_once __DIR__ . '/../config/config.php';
 
 // Include the routes file
-require_once __DIR__ . '/../app/routes.php';
-
+require_once __DIR__ . '/../app/Routes.php';
 
 // Run the admin_insert migration
 $migration = new admin_insert();
